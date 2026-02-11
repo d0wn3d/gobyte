b1c9f1cb9b2f6ea00f0b6f2e0472e9d2d221ec7b
D0WN3D <32816747+D0WN3D@users.noreply.github.com>
2026-02-11 20:42:28 +0200

    fix: remove X11 - add Neoscryt stuff

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -28,6 +28,9 @@ public:
     uint32_t nBits;
     uint32_t nNonce;
 
+    // memory only
+    uint256 powHash;
+
     CBlockHeader()
     {
         SetNull();
@@ -53,11 +56,17 @@ public:
         nTime = 0;
         nBits = 0;
         nNonce = 0;
+        powHash.SetNull();
     }
 
     bool IsNull() const
     {
         return (nBits == 0);
+    }
+
+    void SetCache(const uint256& cache) const
+    {
+        memcpy((void*)&powHash, (const void*)&cache, 32);
     }
 
     uint256 GetHash() const;
@@ -86,14 +95,14 @@ public:
     CBlock(const CBlockHeader &header)
     {
         SetNull();
-        *(static_cast<CBlockHeader*>(this)) = header;
+        *((CBlockHeader*)this) = header;
     }
 
     ADD_SERIALIZE_METHODS;
 
     template <typename Stream, typename Operation>
     inline void SerializationOp(Stream& s, Operation ser_action) {
-        READWRITEAS(CBlockHeader, *this);
+        READWRITE(*(CBlockHeader*)this);
         READWRITE(vtx);
     }
 
