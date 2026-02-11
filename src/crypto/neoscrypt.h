e545774421dc6877c520bd0c9fd2c3d9bd12af2c
D0WN3D <32816747+D0WN3D@users.noreply.github.com>
2026-02-11 21:13:06 +0200

    fix: macros moved inside the !(__cplusplus) block

--- src/crypto/neoscrypt.h
+++ src/crypto/neoscrypt.h
@@ -3,11 +3,11 @@ extern "C" {
 #endif
 
 void neoscrypt(const unsigned char *password, unsigned char *output,
-  unsigned int profile);
+               unsigned int profile);
 
 void neoscrypt_blake2s(const void *input, const unsigned int input_size,
-  const void *key, const unsigned char key_size,
-  void *output, const unsigned char output_size);
+                       const void *key, const unsigned char key_size,
+                       void *output, const unsigned char output_size);
 
 void neoscrypt_copy(void *dstp, const void *srcp, unsigned int len);
 void neoscrypt_erase(void *dstp, unsigned int len);
@@ -15,19 +15,19 @@ void neoscrypt_xor(void *dstp, const void *srcp, unsigned int len);
 
 #if defined(ASM) && defined(MINER_4WAY)
 void neoscrypt_4way(const unsigned char *password, unsigned char *output,
-  unsigned char *scratchpad);
+                    unsigned char *scratchpad);
 
 #ifdef SHA256
 void scrypt_4way(const unsigned char *password, unsigned char *output,
-  unsigned char *scratchpad);
+                 unsigned char *scratchpad);
 #endif
 
 void neoscrypt_blake2s_4way(const unsigned char *input,
-  const unsigned char *key, unsigned char *output);
+                            const unsigned char *key, unsigned char *output);
 
 void neoscrypt_fastkdf_4way(const unsigned char *password,
-  const unsigned char *salt, unsigned char *output, unsigned char *scratchpad,
-  const unsigned int mode);
+                            const unsigned char *salt, unsigned char *output,
+                            unsigned char *scratchpad, const unsigned int mode);
 #endif
 
 unsigned int cpu_vec_exts(void);
@@ -36,15 +36,7 @@ unsigned int cpu_vec_exts(void);
 }
 #endif
 
-/* These must be outside the C++ check to be visible, but guarded to prevent the error */
-#ifndef ROTL32
-#define ROTL32(a,b) (((a) << (b)) | ((a) >> (32 - b)))
-#endif
-
-#ifndef ROTR32
-#define ROTR32(a,b) (((a) >> (b)) | ((a) << (32 - b)))
-#endif
-
+/* EVERYTHING BELOW THIS LINE IS HIDDEN FROM C++ COMPILER (hash.cpp) */
 #if !(__cplusplus)
 
 typedef unsigned long long ullong;
@@ -65,9 +57,18 @@ typedef unsigned char uchar;
 
 typedef uchar hash_digest[DIGEST_SIZE];
 
+/* Macros moved here to prevent collision with hash.cpp functions */
+#ifndef ROTL32
+#define ROTL32(a,b) (((a) << (b)) | ((a) >> (32 - b)))
+#endif
+
+#ifndef ROTR32
+#define ROTR32(a,b) (((a) >> (b)) | ((a) << (32 - b)))
+#endif
+
 #define U8TO32_BE(p) \
     (((uint)((p)[0]) << 24) | ((uint)((p)[1]) << 16) | \
-    ((uint)((p)[2]) <<  8) | ((uint)((p)[3])))
+     ((uint)((p)[2]) <<  8) | ((uint)((p)[3])))
 
 #define U32TO8_BE(p, v) \
     (p)[0] = (uchar)((v) >> 24); (p)[1] = (uchar)((v) >> 16); \
